575fc2aa94f2fae8ed98307a3328cee2bc8eb875
Joo <rudwn3166@naver.com>
2021-10-25 21:17:45 +0900

    첫번째 프레그먼트 작성완료

--- app/src/main/java/com/example/fragment_practice/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/fragment_practice/fragment/FirstFragment.kt
@@ -1,4 +1,33 @@
 package com.example.fragment_practice.fragment
 
-class FirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragment_practice.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_first,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeButton.setOnClickListener {
+
+            changeText.text = "문구가 변경 되었습니다"
+
+        }
+
+    }
+
 }
